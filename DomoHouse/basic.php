a5668745a38ea34ff9eccca433484505c07d505a
invisibleman2403 <invisible_man2403@hotmail.com>
2016-07-07 22:48:02 -0500

    Archivos 1er Sprint

---
+++ DomoHouse/basic.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DomoHouse FCC - Tu casa inteligente al mejor precio</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+
+  <body>
+&nbsp;
+
+  	<div class="row" id="header">
+  		<div class="small-3 columns large-4 columns" id="logo" > 
+  			<img src="images/logoJPG.jpg" alt="" height="200" width="200" >
+        &nbsp;
+        &nbsp;
+  		</div>
+
+  		<div class="small-7 centered columns large-5 centered columns" id="usrOptions"> 
+       <h1><Tit><Sombra>Basic</Sombra></Tit></h1>
+      </div>
+
+      <div class="small-2 columns large-3 columns" id="loginForm"> 
+        <label>Usuario
+          <input type="text" placeholder="User">
+        </label>
+        <label>Contraseña
+          <input type="text" placeholder="******">
+        </label>
+       
+
+      </div>
+
+  	</div>
+    &nbsp;
+    <div class="row" id="tabs">
+      <ul class="menu">
+          <li class="active"><a>Inicio</a></li>
+          <li><a href="d.html">¿Qué hacemos?</a> </li>
+          <li><a href="d.html">Mi casa</a> </li>
+          <li><a>Contacto</a></li>
+        </ul>
+    </div>
+&nbsp;
+     <div class="row" id="bannerfcc">
+      
+      <div class="small-12 centered columns large-12 centered columns" id="fcc">
+        <img src="images/fcc.png" alt="" height="900" width="450" >
+      </div>
+  
+    </div>
+    &nbsp;
+  	<div class="row" id="visor">
+  		<div class="small-4 columns large-3 columns" id="panelmain"> 
+  			<div class="callout">
+       <h3>Inicio</h3>
+       <p align="justify">DomoHouse es un proyecto de los alumnos de la materia Proyectos I+D II de la Facultad de Ciencias de la Computacion y Ciencias de la Electronica</p>
+
+        </div>
+  		</div>
+		<div class="small-8 columns large-9 columns" id="deviceview"> 
+			<div class="callout">
+      <h3>Video demostrativo</h3>
+        <div class="flex-video">
+          <div class="flex-video">
+    <div class="flex-video">
+  <iframe width="420" height="315" src="https://www.youtube.com/embed/Tu-yXNSdaws?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+</div>
+</div>
+        </div>
+      </div>
+		</div>
+  	</div>
+
+   
+
+
+
+
+  	<script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+    <script type="text/javascript">
+      $(window).load(function() {$('#featured').orbit();});
+    </script>
+
+  </body>
+
+  </html>
---
+++ DomoHouse/top.php
@@ -0,0 +1,23 @@
+  	<div class="row" id="header">
+  		<div class="small-3 columns large-4 columns" id="logo" > 
+  			<img src="images/logoJPG.jpg" alt="" height="200" width="200" >
+        &nbsp;
+        &nbsp;
+  		</div>
+
+  		<div class="small-7 centered columns large-5 centered columns" id="usrOptions"> 
+       <h1><Tit><Sombra>DomoHouse FCC</Sombra></Tit></h1>
+      </div>
+
+      <div class="small-2 columns large-3 columns" id="loginForm"> 
+        <label>Usuario
+          <input type="email" placeholder="Usuario">
+        </label>
+        <label>Contraseña
+          <input type="password" placeholder="*****">
+        </label>
+        
+
+      </div>
+
+  	</div>
